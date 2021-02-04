86cf004fcc3c857209b98bdd949137fc72303df6
KIM-DO-WON <55446103+KIM-DO-WON@users.noreply.github.com>
2021-02-04 14:04:14 +0900

    Fix errors in resnet_test.py file

--- resnet_test.py
+++ resnet_test.py
@@ -63,7 +63,7 @@ class ResNet(tf.keras.Model):
 
         self.res3 = ResnetLayer(32, (64, 64), (3, 3))
 
-        self.flatten = tf.keras.Flatten()
+        self.flatten = tf.keras.layers.Flatten()
         self.dense1 = tf.keras.layers.Dense(128, activation='relu')
         self.dense2 = tf.keras.layers.Dense(10, activation='softmax')
 
@@ -73,9 +73,9 @@ class ResNet(tf.keras.Model):
         x = self.conv1(x)
 
         x = self.res1(x, training=training)
-        x- self.pool1(x)
+        x = self.pool1(x)
         x = self.res2(x, training=training)
-        x - self.pool2(x)
+        x = self.pool2(x)
         x = self.res3(x, training=training)
 
         x = self.flatten(x)
@@ -92,9 +92,9 @@ def train_step(model, images, labels, loss_object, optimizer, train_loss, train_
         predictions = model(images, training=True)
         loss = loss_object(labels, predictions)
 
-    gradients = tape.gradient(loss, model.trainable_variable)
+    gradients = tape.gradient(loss, model.trainable_variables)
 
-    optimizer.apply_gradients(zip(gradients, model.trainable_variable))
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
     train_loss(loss)
     train_accuracy(labels, predictions)
 
@@ -118,3 +118,31 @@ x_test = x_test[..., tf.newaxis].astype(np.float32)
 
 train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(10000).batch(32)
 test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
+
+######### Model Test
+model = ResNet()
+
+# 손실함수 정의 및 최적화 기법 정의
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy()
+optimizer = tf.keras.optimizers.Adam()
+
+# 평가지표 정의
+train_loss = tf.keras.metrics.Mean(name="train_loss")
+train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name="train_accuracy")
+
+test_loss = tf.keras.metrics.Mean(name="test_loss")
+test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name="test_accuracy")
+
+for epoch in range(EPOCHS):
+    for images, labels in train_ds:
+        train_step(model, images, labels, loss_object, optimizer, train_loss, train_accuracy)
+
+    for test_images, test_labels in test_ds:
+        test_step(model, test_images, test_labels, loss_object, test_loss, test_accuracy)
+
+    template = "Epoch {}, Loss {}, Accuracy: {}, Test Loss: {}, Test Accuracy: {}"
+    print(template.format(epoch+1,
+                          train_loss.result(),
+                          train_accuracy.result() * 100,
+                          test_loss.result(),
+                          test_accuracy.result() * 100))
