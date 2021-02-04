e27a861741a366f978a94f06f9e8582b21a41c7e
KIM-DO-WON <55446103+KIM-DO-WON@users.noreply.github.com>
2021-02-04 11:58:36 +0900

    Ready to mnist dataset

--- resnet_test.py
+++ resnet_test.py
@@ -106,3 +106,15 @@ def test_step(model, images, labels, loss_object, test_loss, test_accuracy):
     t_lost = loss_object(labels, predictions)
     test_loss(t_lost)
     test_accuracy(labels, predictions)
+
+# dataset
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+x_train = x_train[..., tf.newaxis].astype(np.float32)
+x_test = x_test[..., tf.newaxis].astype(np.float32)
+
+train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(10000).batch(32)
+test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
